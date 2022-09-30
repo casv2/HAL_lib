b13ec2a12b9b1fe7980159926cf178193b38bac3
casv2 <casv2@cam.ac.uk>
2022-09-30 14:01:54 +0100

    added virials

--- lsq.py
+++ lsq.py
@@ -13,7 +13,7 @@ convert = Main.eval("julip_at(a) = JuLIP.Atoms(a)")
 ASEAtoms = Main.eval("ASEAtoms(a) = ASE.ASEAtoms(a)")
 
 def assemble_lsq(ace_basis, atoms_list):
-    num_obs = np.sum([1 + len(at)*3 for at in atoms_list])
+    num_obs = np.sum([1 + len(at)*3 + 9 for at in atoms_list])
     len_ace_basis = Main.eval("length(B)")
 
     Psi = np.zeros((num_obs, len_ace_basis))
@@ -30,9 +30,9 @@ def assemble_lsq(ace_basis, atoms_list):
         Y[i:i+Frows] = np.array(at.arrays["forces"]).flatten()
         i += Frows
 
-        # Vrows = 6
-        # Psi[i:i+Vrows, :] = np.reshape(np.array(virial(ace_basis, convert(ASEAtoms(at)))).flatten(), (len_ace_basis, Vrows)).transpose()
-        # Y[i:i+Vrows] = np.array(at.arrays["virial"]).flatten()
+        Vrows = 9
+        Psi[i:i+Vrows, :] = np.reshape(np.array(virial(ace_basis, convert(ASEAtoms(at)))).flatten(), (len_ace_basis, Vrows)).transpose()
+        Y[i:i+Vrows] = np.array(at.info["virial"]).flatten()
 
     return Psi, Y
 
@@ -42,7 +42,6 @@ def fit(E0s, B, atoms_list, solver, ncomms=32):
     solver.fit(Psi, Y)
     c = solver.coef_
     sigma = solver.sigma_
-    #min_sigma_eig_val = np.min(np.isreal(np.linalg.eigvals(sigma)))
 
     comms = np.random.multivariate_normal(c, sigma, size=ncomms)
     IP = ace_basis.combine(E0s, B, c, comms)
