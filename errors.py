5f579aee9f7fc1aa0ebaa507fbb230715ec3a57a
casv2 <casv2@cam.ac.uk>
2022-10-08 16:49:04 +0100

    inds fix

--- errors.py
+++ errors.py
@@ -14,9 +14,10 @@ def print_errors(IP, al, data_keys):
 
     F_DFT = np.array(F_DFT)
     F_ACE = np.array(F_ACE)
+    inds = F_DFT[F_DFT < data_keys["Fmax"]]
 
     E_RMSE = np.sqrt(np.mean(np.power(np.array(E_DFT) - np.array(E_ACE), 2)))
-    F_RMSE = np.sqrt(np.mean(np.power(F_DFT[F_DFT < data_keys["Fmax"]] - F_ACE[F_ACE < data_keys["Fmax"]], 2)))
+    F_RMSE = np.sqrt(np.mean(np.power(F_DFT[inds] - F_ACE[inds], 2)))
 
     print("============================================")
     print("|  E   : {} meV/at |  F  :  {}  eV/A |".format(str(E_RMSE*1E3)[:5], str(F_RMSE)[:5]))
