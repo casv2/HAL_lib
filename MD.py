e23c179ec9aed84c95cb928a8da5587856347e29
casv2 <casv2@cam.ac.uk>
2023-01-13 15:59:49 +0100

    update vstack

--- MD.py
+++ MD.py
@@ -23,11 +23,6 @@ def VelocityVerlet(ACE_IP, CO_IP, at, dt, tau, baro_settings, thermo_settings):
         forces = at.arrays["HAL_forces"]
     else:
         F_bar, F_bias, F_bar_norms, F_bias_norms, dFn  = CO_IP.get_property('force_data',  at) 
-        #print("==")
-        #print(np.array(F_bar))
-        #print("==")
-        #print(np.array(F_bias))
-        print(np.vstack(F_bar))
         forces = np.vstack(F_bar) - tau * np.vstack(F_bias)
 
     p = at.get_momenta() + 0.5 * dt * forces
@@ -41,7 +36,6 @@ def VelocityVerlet(ACE_IP, CO_IP, at, dt, tau, baro_settings, thermo_settings):
     at.set_positions(r + dt * p / masses)
 
     F_bar, F_bias, F_bar_norms, F_bias_norms, dFn  = CO_IP.get_property('force_data',  at) 
-    #forces = np.array(F_bar) - tau * np.array(F_bias)
     forces = np.vstack(F_bar) - tau * np.vstack(F_bias)
     at.arrays["HAL_forces"] = forces
 
