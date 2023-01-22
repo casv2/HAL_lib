f0d6d7fb8810ee952853bcbcf9d7993e68d8337b
casv2 <casv2@cam.ac.uk>
2023-01-22 12:26:55 +0000

    heuristics for r_0 and r_in

--- BO_optim.py
+++ BO_optim.py
@@ -13,9 +13,10 @@ def BO_basis_optim(optim_basis_param, solver, atoms_list, E0s, data_keys, weight
     elements = optim_basis_param["elements"]
     max_len_B = optim_basis_param["max_len_B"]
 
-    distances_all = np.array([ at.get_all_distances(mic=True) for at in atoms_list]).flatten()
-    distances_first_shell = distances_all[ distances_all <= 4.0]
+    distances_all = np.hstack([ at.get_all_distances(mic=True).flatten() for at in atoms_list])
+    distances_first_shell = distances_all[ distances_all <= 3.5]
     distances_non_zero = distances_first_shell[distances_first_shell != 0.0] 
+    
     r_in = np.min(distances_non_zero)
 
     x,y = np.histogram(distances_non_zero, bins=100)
@@ -29,12 +30,11 @@ def BO_basis_optim(optim_basis_param, solver, atoms_list, E0s, data_keys, weight
         """return the f1-score"""
         cor_order = trial.suggest_int('cor_order', low=2, high=3, step=1)
 
-        #r_0 = trial.suggest_float('r_0', low=1.0, high=4.0, step=0.1)
-        r_cut_ACE = trial.suggest_float('r_cut_ACE', low=4.5, high=6.0, step=0.1)
+        r_cut_ACE = trial.suggest_float('r_cut_ACE', low=4.5, high=2*r_0, step=0.1)
+        r_cut_pair = trial.suggest_float('r_cut_pair', low=2*r_0, high=3*r_0, step=0.1)
 
         maxdeg = trial.suggest_int('maxdeg', low=4, high=12, step=1)
         poly_deg_pair = trial.suggest_int('poly_deg_pair', low=7, high=12, step=1)
-        r_cut_pair = trial.suggest_float('r_cut_pair', low=6.0, high=8.0, step=0.1)
 
         basis_info = {
         "elements" : elements, 
